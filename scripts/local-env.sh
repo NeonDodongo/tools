0caaf4aae3832775d9a993c8fc5619c97c48f229
neondodongo <neondodongo@gmail.com>
2020-07-23 01:22:04 -0500

    refactor local-env.sh

--- scripts/local-env.sh
+++ scripts/local-env.sh
@@ -1,59 +1,80 @@
 #!/bin/bash
 
 # Add additional image names here, do not specify tag
-declare -a IMAGES=("alpine" "minio/minio" "mongo" "bitnami/kubectl")
+declare -a IMAGES=("minio/minio" "mongo" "bitnami/kubectl")
 
 # Script Vars
 TAG=latest
-DOCKER_IP=localhost
 UNAME=$(uname)
+DOCKER_MACHINE=dev
 
-# Determine OS and docker IP
-if [[ ${UNAME} == CYGWIN* || ${UNAME} == MINGW* ]]; then
-    echo "Running on Windows OS...changing DOCKER_IP var"
-    DOCKER_IP=$(docker-machine ip)
-fi
+# Determine Docker Machine IP
+echo "Retrieving Docker Machine IP for '${DOCKER_MACHINE}'"
+DOCKER_IP=$(docker-machine ip ${DOCKER_MACHINE})
 
-# Check if Docker is installed on system
+# Check if Docker is available
 echo ""
 echo "Checking docker version..."
 docker --version
 if [ $? != 0 ]; then
-    echo "Docker not found on system...aborting"
+    echo "...docker not found on system...aborting"
     exit 1
 fi
 
 # Kill running containers for mongo and minio
 # Add kill commands for containers spun up by this script
 echo ""
-echo "Killing running containers..."
-docker kill $(docker ps -q --filter ancestor=mongo)
-docker kill $(docker ps -q --filter ancestor=minio/minio)
+echo "Removing possible running containers..."
+for i in "${IMAGES[@]}"; do
+    echo ${i}...
+    if ! docker rm -f $(docker ps -q --filter ancestor=${i}) &> /dev/null; then
+        echo "...no ancenstor found for '${i}'"
+    fi
+done
 
 # Pull latest images for local development environment
 echo ""
 echo "Pulling latest images..."
 for i in "${IMAGES[@]}"; do
     image=${i}:${TAG}
-    docker pull ${image}
-    if [ $? != 0 ]; then
-        echo "Failed to pull image: ${image}"
+
+    if ! docker pull ${image}; then 
+        echo "...failed to pull image: ${image}"
     fi
 done
 
-# Spin up docker containers for mongo and minio
+start_mongo_container() {
+    image=$1
+    echo ${i}...
+    if ! docker run -d -p 27017:27017 ${image} &>/dev/null; then
+        echo "...failed to start container for image ${image}"
+        return
+    fi
+    echo "...created!"
+}
+
+start_minio_container() {
+    image=$1
+    echo ${i}...
+    if ! docker run -e "MINIO_ACCESS_KEY=TEST-USER" -e "MINIO_SECRET_KEY=TEST-USER" -d -p 9000:9000 ${image} server start &>/dev/null; then
+        echo "...failed to start container for image ${image}"
+        return
+    fi
+    echo "...created!"
+}
+
+# Spin up specified docker containers
 echo ""
 echo "Spinning up docker containers..."
 for i in "${IMAGES[@]}"; do
     image=${i}:${TAG}
-    if [[ ${image} == *"mongo"* ]]; then
-        docker run -d -p 27017:27017 ${image}
-        echo "Created local mongo instance running inside docker container"
-    fi
-    if [[ ${image} == *"minio"* ]]; then
-        docker run -e "MINIO_ACCESS_KEY=TEST-USER" -e "MINIO_SECRET_KEY=TEST-USER" -d -p 9000:9000 ${image} server start
-        echo "Created local minio instance running inside docker container"
-    fi
+
+    case ${image} in
+    *"mongo"*)
+        start_mongo_container ${image};;
+    *"minio"*)
+        start_minio_container ${image};;
+    esac
 done
 
 # Clean up dangling artifacts
@@ -61,5 +82,8 @@ echo ""
 echo "Cleaning up dangling artifacts..."
 echo "y" | docker system prune
 
-echo "*********************************"
+echo "************************************************************************"
 echo "Docker containers can be accessed via ${DOCKER_IP}:<port_of_container>"
+echo ""
+
+docker ps -a
